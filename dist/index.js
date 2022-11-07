0d1074432e0bbb42f52091b4454747555cc75f96
wandera7 <julius.wandera@student.moringaschool.com>
2022-11-07 10:28:46 +0300

    Typescript initial

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("joe boy");
+let i = 5;
+if (i < 7) {
+    i += 8;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("joe boy");
+let i: number= 5;
+if(i<7){
+    i+=8;
+}
